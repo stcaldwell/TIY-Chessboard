0b88c9079fe7fa7d258ef08543098a834ebfafaa
Sam Caldwell <caldwell.s.thorne@gmail.com>
2017-06-13 16:49:48 -0400

    Add id's to images and correct numbering in array.

--- main.js
+++ main.js
@@ -15,11 +15,11 @@
 // document.body.appendChild(img);
 
 var board = [
-  ['BC','BKn','BB','BQ','K','BB','BKn','BC'],
-  ['BP','BP','BP','BP','BP','BP','BP','BP'],
+  ['BC1','BKn1','BB1','BQ','K','BB2','BKn2','BC2'],
+  ['BP1','BP2','BP3','BP4','BP5','BP6','BP7','BP8'],
   [' ',' ',' ',' ',' ',' ',' ',' '],
   [' ',' ',' ',' ',' ',' ',' ',' '],
   [' ',' ',' ',' ',' ',' ',' ',' '],
   [' ',' ',' ',' ',' ',' ',' ',' '],
-  ['Wp','Wp','Wp','Wp','Wp','Wp','Wp','Wp'],
-  ['WC','WKn','WB','WQ','WK','WB','WKn','WC'] ]
+  ['WP1','WP2','WP3','WP4','WP5','WP6','WP7','WP8'],
+  ['WC1','WKn1','WB1','WQ','WK','WB2','WKn2','WC2'] ]
