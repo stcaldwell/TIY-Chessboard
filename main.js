670433a890822cf616ec275c49fe7899ea080d1d
Sam Caldwell <caldwell.s.thorne@gmail.com>
2017-06-13 15:51:41 -0400

    add board array and id's to pieces.

---
+++ main.js
@@ -0,0 +1,25 @@
+// set id's for the images.
+
+// link images to javascript in proper board position.
+
+// create an array of board. Using image variables.
+
+// link one movement to step forward button.
+
+
+// var img = new Image();
+// img.src = 'image.png';
+// img.onclick = function() {
+//     window.location.href = 'http://putyourlocationhere/';
+// };
+// document.body.appendChild(img);
+
+var board = [
+  ['BC','BKn','BB','BQ','K','BB','BKn','BC'],
+  ['BP','BP','BP','BP','BP','BP','BP','BP'],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  ['Wp','Wp','Wp','Wp','Wp','Wp','Wp','Wp'],
+  ['WC','WKn','WB','WQ','WK','WB','WKn','WC'] ]
